fd2481dbb5584e7b673993966d06e45823e4c80b
AllenSWQ <44560055+AllenSWQ@users.noreply.github.com>
2018-10-29 14:25:19 +0800

    Add files via upload

---
+++ src/main/java/com/portal/constant/Constant.java
@@ -0,0 +1,19 @@
+package com.portal.constant;
+
+public class Constant {
+	public static String separate = "/";
+	public static String apk_url_no_prefix = "/self_upgrade";
+	
+	public static String file_url = "/data01/apps/portal/channel_file";
+	public static String file_url_no_prefix = "/channel_file";
+	
+	public static String st_file_url = "/data01/apps/portal/st_data";
+	public static String st_file_name = "device_data.csv";
+
+	/**
+	 * 邮件验证码发送标识
+	 */
+	public static final String SECURITY_CODE = "_FR_SECURITY_CODE";
+	
+	public static final String IP = "127.0.0.1";
+}
